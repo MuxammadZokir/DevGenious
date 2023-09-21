bd8f0f0aaa34aca826a2b02904b573602834ab55
MuxammadZokir <alijonovmf1306@gmail.com>
2023-09-21 16:23:37 +0500

    Create ISubjectService

---
+++ DevGenious.Service/Interfaces/ISubjectService.cs
@@ -0,0 +1,12 @@
+﻿using DevGenious.Service.DTOs;
+
+namespace DevGenious.Service.Interfaces;
+
+public interface ISubjectService
+{
+    public Task<bool> RemoveAsync(long id);
+    public Task<List<SubjectForResultDto>> GetAllAsync();
+    public Task<SubjectForResultDto> GetByIdAsync(long id);
+    public Task<SubjectForResultDto> UpdateAsync(SubjectForUpdateDto dto);
+    public Task<SubjectForResultDto> CreateAsync(SubjectForCreationDto dto);
+}
